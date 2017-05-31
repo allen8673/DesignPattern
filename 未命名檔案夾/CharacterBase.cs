ca163a5e8cb2eed6e46f9fce97af9f747d6454cd
Allen_Lin <allen8673@gmail.com>
2017-06-01 02:01:12 +0800

    test

---
+++ 未命名檔案夾/CharacterBase.cs
@@ -0,0 +1,17 @@
+﻿using Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public abstract class CharacterBase : ICharacter
+    {
+        public abstract string Describe { get; }
+        public abstract int CurrentHP { get; internal set; }
+        public abstract int FullHP { get; }
+        public abstract IMove[] MoveList { get; }
+    }
+}
